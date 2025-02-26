360bca6a51577710dd18a1d9fe39d53e8d94d4c5
Gokul <75162131+Gokul2003g@users.noreply.github.com>
2025-02-26 15:34:42 +0530

    Sprint 1 done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken"
+import { middleware } from "./middleware";
+import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types"
 
 const app = express();
 const port = 3000;
@@ -7,16 +9,33 @@ const port = 3000;
 app.listen(port);
 
 app.post('/signup', (req, res) => {
-  res.send("Hello World!")
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({ message: "Incorrect Inputs for Signup" })
+    return;
+  }
+
+  res.json({ userId: "123" })
 })
 
 app.post('/singin', (req, res) => {
+  const data = SigninSchema.safeParse(req.body)
+  if (!data.success) {
+    res.json({ message: "Incorrect inputs for signin" })
+    return;
+  }
   res.send("Signin route!")
 })
 
-app.post("/room", (req, res) => {
-
+app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body)
+  if (!data.success) {
+    res.json({ message: "Incorrect inputs for Room Creation" })
+    return;
+  }
+  res.json({ roomID: 123 })
 })
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 })
